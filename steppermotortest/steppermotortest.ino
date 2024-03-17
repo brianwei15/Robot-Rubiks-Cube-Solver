ea354ee08e447018db3075fd05600ee453ec7d6d
Brian Wei <brian.wei1113@gmail.com>
2024-03-17 14:18:01 -0700

    getting ready for demo

--- steppermotortest/steppermotortest.ino
+++ steppermotortest/steppermotortest.ino
@@ -13,18 +13,19 @@
 #define dirPin1 4
 #define EN1 2
 
-#define stepPin2 6
-#define dirPin2 7
-#define EN2 5
+// #define stepPin2 6
+// #define dirPin2 7
+// #define EN2 5
 
 void setup() {
   // Sets the three pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(EN, OUTPUT);
-  digitalWrite(EN, LOW);  // Setting EN to low enables the motor to turn
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(EN1, OUTPUT);
+  digitalWrite(EN1, LOW);  // Setting EN to low enables the motor to turn
   Serial.begin(9600);
 }
+
 void loop() {
   int moves[5] = {1, 0, 1, 1, 0};
   int size = sizeof(moves)/sizeof(int);
@@ -35,16 +36,16 @@ void turn(int dir) {
   //1 is clockwise (right)
   //0 is counterclockwise (left)
   if (dir == 1) {
-    digitalWrite(dirPin, HIGH); // clockwise
+    digitalWrite(dirPin1, HIGH); // clockwise
   }
   else {
-    digitalWrite(dirPin, LOW); // counterclockwise
+    digitalWrite(dirPin1, LOW); // counterclockwise
   }
-  // 400 pulses for 90 degree turn
+  // 405 pulses for 90 degree turn
   for (int x = 0; x < 405; x++) {
-      digitalWrite(stepPin, HIGH);
+      digitalWrite(stepPin1, HIGH);
       delayMicroseconds(200);  // by changing this time delay between the steps we can change the rotation speed
-      digitalWrite(stepPin, LOW);
+      digitalWrite(stepPin1, LOW);
       delayMicroseconds(200);
   }
   delay(300); // 0.2 second delay
