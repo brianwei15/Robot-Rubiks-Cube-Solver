d4b066cd3e749f741ee173a586ea84c5c07d8a5c
Brian Wei <brian.wei1113@gmail.com>
2024-05-16 15:10:26 -0700

    new solutionstring mapping

--- TwoPhaseK/kociemba.py
+++ TwoPhaseK/kociemba.py
@@ -4,4 +4,45 @@ import twophase.solver  as sv
 
 cubestring = 'DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'
 
-print(sv.solve(cubestring,19,2))
+moves = sv.solve(cubestring, 19, 2)
+
+nummoves = moves[-4:-2] #the number of moves needed to solve the cube
+print("num of moves: " + nummoves)
+movesstring = moves[:-6] #substring the last part of the string which outputs the number of moves it will take
+newmoves = "" #new formatted string
+
+#map characters in moves to new string in the format of motornum + move direction
+for character in movesstring:
+    match character:
+        case "U":
+            newmoves += "1"
+            continue
+        case "R":
+            newmoves += "2"
+            continue
+        case "F":
+            newmoves += "3"
+            continue
+        case "D":
+            newmoves += "4"
+            continue
+        case "L":
+            newmoves += "5"
+            continue
+        case "B":
+            newmoves += "6"
+            continue
+        case "1": #clockwise
+            newmoves += "0 "
+            continue
+        case "2": #180degrees
+            newmoves += "2 "
+            continue
+        case "3": #counterclockwise
+            newmoves += "1 "
+            continue
+        case default:
+            continue
+
+
+print(newmoves)
