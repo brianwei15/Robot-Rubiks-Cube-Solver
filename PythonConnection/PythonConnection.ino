446e7a0c69efc43135fc16da53cef94012c36790
Josephc1123 <161387320+Josephc1123@users.noreply.github.com>
2024-05-22 15:40:33 -0700

    Add files via upload
    New working python to arduino connection

---
+++ PythonConnection/PythonConnection.ino
@@ -0,0 +1,44 @@
+void setup() {
+  // Enables all six motors
+  for (int i = 0; i < 6; i++) {
+    pinMode(3 + 3 * i, OUTPUT);
+    pinMode(4 + 3 * i, OUTPUT);
+    pinMode(2 + 3 * i, OUTPUT);
+    digitalWrite(2 + 3 * i, LOW);
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // This loop now just waits for incoming serial data.
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    int command = Serial.parseInt(); // Read integer command from Serial
+    if (command != 0) {
+      int motor = command / 10;
+      int dir = command % 10;
+      turn(dir, motor);
+      Serial.print("Motor ");
+      Serial.print(motor);
+      Serial.print(": ");
+      Serial.println(dir == 1 ? "clockwise" : "counterclockwise");
+    }
+  }
+}
+
+void turn(int dir, int motor) {
+  int stepPin = 3 + 3 * (motor - 1);
+  int dirPin = 4 + 3 * (motor - 1);
+
+  digitalWrite(dirPin, dir == 1 ? HIGH : LOW); // Set direction
+
+  for (int x = 0; x < 400; x++) { // Perform steps
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(200);
+  }
+}
