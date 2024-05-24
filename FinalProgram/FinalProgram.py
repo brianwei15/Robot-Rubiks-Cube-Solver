10eff130b8a94f406a9d13d4267ad5a979603c5f
Brian Wei <brian.wei1113@gmail.com>
2024-05-24 14:26:33 -0700

    minor stuff

--- FinalProgram/FinalProgram.py
+++ FinalProgram/FinalProgram.py
@@ -258,7 +258,7 @@ print(newmoves)
 import serial
 import time
 
-ser = serial.Serial('COM5', 9600, timeout=1)
+ser = serial.Serial('COM6', 9600, timeout=1)
 time.sleep(2)  
 
 def send_commands(commands):
